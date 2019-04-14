381702fb3f5bdce2f332517228ad36530dc28d80
Jackson Dumas <tylerdumas3@hotmail.com>
2019-04-13 19:21:08 -0500

    added hover script

---
+++ RowdyHacks/Assets/Scripts/HoverOverCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverOverCard : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField]
+    private Animator animator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Tits");
+    }
+}
