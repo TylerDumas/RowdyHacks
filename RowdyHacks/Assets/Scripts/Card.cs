571587c1629684ffc410cc91675d9274befe074b
Jackson Dumas <tylerdumas3@hotmail.com>
2019-04-14 03:59:41 -0500

    gave card subclasses their own files

--- RowdyHacks/Assets/Scripts/Card.cs
+++ RowdyHacks/Assets/Scripts/Card.cs
@@ -12,21 +12,3 @@ public abstract class Card : ScriptableObject
     public Sprite artwork;  //Character drawing
 
 }
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Generic Card")]
-public class GenericCard: Card{
-    public string type = "Generic";
-
-}
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Knight Card")]
-public class KnightCard : Card
-{     //Card for Knight
-    public string type = "Knight";
-}
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Ranger Card")]
-public class RangerCard : Card {    //Card for Ranger
-    public string type = "Ranger";
-}
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Healer Card")]
-public class HealerCard : Card {    //Card for Healer
-    public string type = "Healer";
-}
